09f4159054f2d999156e4b8b1525925a9b6b0633
amos-333 <over2amos333@gmail.com>
2022-10-29 11:30:26 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    HomeComponent,
+    LoginComponent,
+    CartComponent,
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
